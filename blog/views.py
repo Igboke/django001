56b99fad278de423ce2737d0e4cf846dc4a8a0bf
Igboke Daniel <danieligboke669@gmail.com>
2024-11-17 23:03:08 +0100

    Updates

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-# Create your views here.
-posts = [
-    {'author': 'Chibuike Daniel',
-     'title': 'This is Blog Post 001',
-     'content':'on',
-     'date_posted':'5-03-24'},
-     {'author':'Jacobi Aviari',
-      'title':'This is Blog Post 003',
-      'content':'off',
-      'date_posted':'5-06-24'}
-]
 def home(requests):
     context = {
         'posts':Post.objects.all()
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(requests):
@@ -8,9 +9,13 @@ def register(requests):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(requests,f'account created for {username} was successful')
-            return redirect('blog-home')
+            messages.success(requests,f'Account created for {username} was successful. You can now login!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(requests,'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
 
