7057bc2bf55cd0b659b7c0c270027d5989ba0e0d
Igboke Daniel <danieligboke669@gmail.com>
2024-11-17 18:11:08 +0100

    further updates

--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(requests):
-    form = UserCreationForm()
+    if requests.method == 'POST':
+        form = UserRegisterForm(requests.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(requests,f'account created for {username} was successful')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
     return render(requests,'users/register.html',{'form':form})
 
