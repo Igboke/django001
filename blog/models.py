a26d36226425ff90ddd3428aa3cbb1297750156b
Igboke Daniel <danieligboke669@gmail.com>
2024-11-19 23:13:11 +0100

    improved update and user experience

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Post(models.Model):
@@ -8,6 +9,9 @@ class Post(models.Model):
     date_posted = models.TimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
+    
     def __str__(self):
         return self.title
 
