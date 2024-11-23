efdfc4c38aadaf3b4b41b758e7f5346cc19066bf
Igboke Daniel <danieligboke669@gmail.com>
2024-11-23 10:59:57 +0100

    added reset password functionality removed sensitive email details for security

--- django001/settings.py
+++ django001/settings.py
@@ -133,3 +133,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL ='blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# removed email host user and password for security reasons
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
