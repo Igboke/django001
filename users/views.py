97d60c72017161726c3c5d3fe53206e5d2a9cc52
Igboke Daniel <danieligboke669@gmail.com>
2024-11-28 21:30:47 +0100

    edited messages

--- users/views.py
+++ users/views.py
@@ -23,7 +23,7 @@ def profile(request):
         if p_form.is_valid() and u_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Account updated was successful. You can now login!')
+            messages.success(request, f'Account update was successful')
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
