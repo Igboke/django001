96cf7d24b5209c8340732cbdbd690f1c1f433ebb
Igboke Daniel <danieligboke669@gmail.com>
2024-11-28 21:11:35 +0100

    improved admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','author','date_posted','status']
+    list_filter = ['status','date_created','date_posted','author']
+    search_fields = ['title','content']
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'published'
+    ordering = ['status','date_posted']
+    show_facets = admin.ShowFacets.ALWAYS
+
+
