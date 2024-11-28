d49b524842ebbc70e287ddd1e50eb8bce5bd7c13
Igboke Daniel <danieligboke669@gmail.com>
2024-11-28 21:32:21 +0100

    added forms in blog app

---
+++ blog/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class EmailPostForms(forms.Form):
+    name = forms.CharField(max_length=30)
+    email = forms.EmailField()
+    to = forms.EmailField
+    comment = forms.CharField(required=False,widget=forms.Textarea)
