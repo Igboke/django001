751d4862af4d0114c658da4b3dde17b123b33039
Igboke Daniel <danieligboke669@gmail.com>
2024-11-28 21:23:54 +0100

    added namespace change post detail url pattern

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views #or just import views?
-from .views import PostListView, PostDetailView,PostCreateView,PostUpdateView,PostDeleteView, UserPostListView
+from .views import PostListView,PostCreateView,PostUpdateView,PostDeleteView, UserPostListView
 
+app_name = 'blog'
 
 urlpatterns = [
     path('',PostListView.as_view(), name='blog-home'),
     path('about/',views.about,name='blog-about'),
     path('user/<str:username>/',UserPostListView.as_view(), name='user-posts'),
-    path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
+    # path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
+    path('post/<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail_view,name='post-detail'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
     path('post/new/',PostCreateView.as_view(), name='post-create')
