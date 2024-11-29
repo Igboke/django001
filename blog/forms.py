7058c768d383d990f8624a75c81652890d616adf
Igboke Daniel <danieligboke669@gmail.com>
2024-11-29 11:34:06 +0100

    added () to 'to' email field

--- blog/forms.py
+++ blog/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class EmailPostForms(forms.Form):
     name = forms.CharField(max_length=30)
     email = forms.EmailField()
-    to = forms.EmailField
+    to = forms.EmailField()
     comment = forms.CharField(required=False,widget=forms.Textarea)
